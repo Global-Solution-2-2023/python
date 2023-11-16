8bcc82318bc517740055f26ebe319fd51f4f7d19
Victor <victormnuzzi003@gmail.com>
2023-11-15 23:54:04 -0300

    função print do menu

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # python
+
+## O que fazer
+
+### 1a Parte - Início 
+- Menu com opções:
+    - Cadastro
+    - Login
+
+### 2a Parte - Menu Principal
+- Menu com opções:
+    - 
+
+## Arquivo Json c
+- Usuários
+    - Usuário:  
+        - nome
+        - email
+        - senha
+        - skills
+
+
--- app.py
+++ app.py
@@ -13,12 +13,23 @@
 import json
 from time import sleep
 from random import randint
+import math
 
-# --------------------- Funções ---------------------
+# --------------------- Funções ---------------------#
+
+# ---------- Opções ---------- #
+menu_options = {
+    "1" : "Personagem",
+    "2" : "Skills",
+    "3" : "Missões",
+    "4" : "Configurações",
+    "5" : "Ajuda",
+    "0" : "Sair"
+}
 
 # Função para definir o tamanho do programa no printdo terminal
 def calcTamanhoPrograma(titulo=''):
-    return len(titulo) * 3 # armazenando em uma variável o tamanho do título para formatação do menu
+    return len(titulo) * 6 # armazenando em uma variável o tamanho do título para formatação do menu
 
 # Função de linha simples para estética
 def linhaSimples(tam):
@@ -45,17 +56,46 @@ def carregandoMenu(menu):
     print()
     sleep(0.5)
 
+# Função para printar um menu
+def printMenu(titulo="",opcoes={},tamanho=calcTamanhoPrograma('Programa X')):
+    frase_maior = ''
+
+    print(f'{titulo.upper():^{tamanho}}')
+    linhaSimples(tamanho)
+
+    for k, v in opcoes.items():
+        frase_nova = f'[{k}] - {v}'
+
+        if len(frase_nova) > len(frase_maior):
+            frase_maior = frase_nova
+
+    centralizar = math.ceil((tamanho - len(frase_maior)) / 2)
+
+    while True:
+
+        for k, v in opcoes.items():
+
+            print(' ' * centralizar, end="")
+
+            print(f"[{k}] - {v}")
+
+        linhaSimples(tamanho)
+
+        escolha_usuario = tratarInput('qual a sua opcao?', len(opcoes))
+        
+        return escolha_usuario
+
 # --------------------- Programa Principal ---------------------
 
-# ---------- Variáveis ----------
+# ---------- Variáveis ---------- #
 
 encerrar_programa = False
 
 # ---------- Loop do Programa ----------
 
 while not encerrar_programa:
-    tamanho_do_programa = calcTamanhoPrograma('Programa X')
 
+    tamanho_do_programa = calcTamanhoPrograma('Programa X')
 
     # ----- Leitura do arquivo JSON -----
 
@@ -64,17 +104,12 @@ while not encerrar_programa:
 
     # ----- Print do Menu Principal -----
     
-    print(f'{"MENU PRINCIPAL":^{tamanho_do_programa}}')
-    linhaSimples(tamanho_do_programa)
-    print(f"{'[1] - Opcao 1':^{tamanho_do_programa}}\n{'[2] - Opcao 2':^{tamanho_do_programa}}\n{'[3] - Opcao 3':^{tamanho_do_programa}}\n{'[4] - Opcao 4':^{tamanho_do_programa}}")
-    linhaSimples(tamanho_do_programa)
+    printMenu("menu principal", menu_options)
 
 
     escolha_principal = inputSublinhado('Selecione uma opção: ')
 
     carregandoMenu('Exemplo')
-
-
 
 
     # ----- Dump para o arquivo JSON -----
